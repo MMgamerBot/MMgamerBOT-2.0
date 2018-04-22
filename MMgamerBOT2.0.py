22b40a591097db95aff8e03a5bd96f1d660a2022
EpicShardGaming YT <35035079+EpicShardGamingYT@users.noreply.github.com>
2018-04-22 19:27:22 +0200

    fixed issue

--- MMgamerBOT2.0.py
+++ MMgamerBOT2.0.py
@@ -19,15 +19,19 @@ async def ping(ctx):
 
 @bot.command(pass_context=True)
 async def info(ctx, user: discord.Member):
-    await bot.say("The users name is: {}".format(user.name))
-    await bot.say("The users ID is: {}".format(user.id))
-    await bot.say("The users status is: {}".format(user.status))
-    await bot.say("The users highest role is: {}".format(user.top_role))
-    await bot.say("The user joined at: {}".format(user.joined_at))
+	embed=discord.Embed(title="Stats for {}".format(user.name), description="Show {} stats".format(user.name), color=0x00ff00)
+	embed.add_field(name="Name: ", value=user.name, inline=False)
+	embed.add_field(name="ID: ", value=user.id, inline=False)
+	embed.add_field(name="Status: ", value=user.id, inline=False)
+	embed.add_field(name="Top role: ", value=user.top_role, inline=False)
+	embed.add_field(name="Joined at: ", value=user.joined_at, inline=False)
 
 @bot.command(pass_context=True)
 async def kick(ctx, user: discord.Member):
     await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
     await bot.kick(user)
 
-bot.run("TOKEN")
+@bot.event
+async def on_message(message):
+	await bot.process_commands(message)
+bot.run("NDM3NjU3MDUzNzU5MDEyODY0.Db5UQA.PEoybWlGnQTzEPB6XfeQhrJFvlM")
