cfaeb845a6bb9b62062af0cbdd2e283c112beda1
EpicShardGaming YT <35035079+EpicShardGamingYT@users.noreply.github.com>
2018-04-28 09:48:52 +0200

    Update MMgamerBOT2.0.py

--- MMgamerBOT2.0.py
+++ MMgamerBOT2.0.py
@@ -74,7 +74,7 @@ async def all_servers(ctx):
         embed = discord.Embed(title="All servers", description="lists all servers the bot is in.", color=0x00ff00)
         tmp = 1
         for i in bot.servers:
-            embed.add_field(name=str(tmp), value=i.name, inline=True)
+            embed.add_field(name=str(tmp), value=i.name, inline=False)
             tmp += 1
         await bot.say(embed=embed)
                   
