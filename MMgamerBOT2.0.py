3ea4770c5b1959ae96a17b07bfed40522db01778
EpicShardGaming YT <35035079+EpicShardGamingYT@users.noreply.github.com>
2018-04-29 13:41:21 +0200

    Update MMgamerBOT2.0.py

--- MMgamerBOT2.0.py
+++ MMgamerBOT2.0.py
@@ -23,7 +23,7 @@ async def on_ready():
     print ("I am running on " + bot.user.name)
     print ("With the ID: " + bot.user.id)
     await bot.change_presence(game=discord.Game(name="mmgamerbot.com", url="https://twitch.tv/EpicShardGaming", type=1))
-    client.loop.create_task(loop())
+    bot.loop.create_task(loop())
     
 @bot.event
 async def on_command_error(message, error):
